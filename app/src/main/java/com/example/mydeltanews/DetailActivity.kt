622c8730b15a9c2c21e5265ac406fe87a99aeb5b
farshad <farshad049@gmail.com>
2022-06-04 13:21:37 +0430

    loading state added

--- app/src/main/java/com/example/mydeltanews/DetailActivity.kt
+++ app/src/main/java/com/example/mydeltanews/DetailActivity.kt
@@ -2,6 +2,8 @@ package com.example.mydeltanews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.example.mydeltanews.databinding.ActivityDetailBinding
 import com.example.mydeltanews.databinding.ActivityMainBinding
@@ -12,9 +14,22 @@ class DetailActivity : AppCompatActivity() {
         val binding:ActivityDetailBinding=
             DataBindingUtil.setContentView(this,R.layout.activity_detail)
 
-        val url=intent.getStringExtra("link") ?: ""
 
+
+        // WebViewClient is a class to take care of loading state fot web pages
+        val client=object : WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.progressBar.hide()
+            }
+        }
+        //set the client object for web view
+        binding.webView.webViewClient=client
+
+        //take data from intent
+        val url=intent.getStringExtra("link") ?: ""
         binding.url=url
 
-    }
+
+
+    }//FUN
 }
